6701e94ba201da71b9ade990d2863976a7b85484
Gaby Trejo <gabytrejo303@gmail.com>
2017-06-11 22:54:29 -0500

    Datas agregados

--- public/js/app.js
+++ public/js/app.js
@@ -3,14 +3,23 @@ $.getJSON("http://pokeapi.co/api/v2/pokemon/", function(response){
     agregarPokemones(pokemones);
 });
 
+
+$(document).ready(function(){
+// the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+});
+
+
 var sprite = 0;
 
 function agregarPokemones(pokemones){
-    pokemones.forEach(function(pokemon){ 
-        var $li = $("<li />");
+    pokemones.forEach(function(pokemon, indice){ 
+        var $li = $("<li />", {"data-indice": indice, "data-url": pokemon.url});
         $li.append($("<div />").css("background", "url(assets/img/pokemones_sprite.png)" + sprite + "px 0"));
         $("#lista_pokemones").append($li.append($("<span />").text(pokemon.name)));
         sprite -= 80;
     });
     
 };
+
+
